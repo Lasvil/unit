a5962eb77ad5167f52a02e2809267e80a00a7421
ArtyomKiyashko <lounershoargent@gmail.com>
2023-08-22 20:27:16 +0300

    Write autotest

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testWith800() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(800);
+        int expected = 200;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWith1100() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1100);
+        int expected = 900;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWith1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assertions.assertEquals(actual, expected);
+    }
+}
